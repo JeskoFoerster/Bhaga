0d90488a5e642467dd2c996a23679d26ec66b02a
JeskoFörster <foersterjesko@gmail.com>
2024-04-26 08:47:04 +0200

    removed keyValStore.c/h because map.c/h does this functionality.

--- keyValStore.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Jesko Förster on 18.04.2024.
-//
-
-
--- keyValStore.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Jesko Förster on 18.04.2024.
-//
-
-#ifndef BHAGA_KEYVALSTORE_H
-#define BHAGA_KEYVALSTORE_H
-
-#endif //BHAGA_KEYVALSTORE_H
