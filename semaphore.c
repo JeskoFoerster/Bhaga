80c9c9bcce88a2bd0a14abea751d28b2aec60a1b
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-04-27 17:07:59 +0200

    wrong file, sorry

--- semaphore.c
+++ semaphore.c
@@ -4,23 +4,11 @@
 
 #include "semaphore.h"
 
-/**
- * Creates a semaphore group with the specified number of semaphores.
- * @param count The number of semaphores to create in the group.
- * @return The semaphore group ID on success, -1 on error.
- */
 int semaphoreCreateGroup(int count){
     int sem_group_id = semget (IPC_PRIVATE, count, IPC_CREAT|0644);
     return sem_group_id;
 }
 
-/**
- * Initializes the values of all semaphores in a semaphore group.
- * @param sem_group_id The ID of the semaphore group.
- * @param sem_id Ignored (for compatibility with future extensions).
- * @param value The initial value to set for all semaphores in the group.
- * @return 0 on success, -1 on error.
- */
 int semaphoreSetValue(int sem_group_id, int sem_id, int value){
     unsigned short marker[1];
     marker[sem_id] = value;
@@ -28,14 +16,6 @@ int semaphoreSetValue(int sem_group_id, int sem_id, int value){
     return result;
 }
 
-/**
- * Decrements the value of a specific semaphore in a semaphore group,
- * potentially blocking the calling process until the value becomes non-negative.
- *
- * @param sem_group_id The ID of the semaphore group.
- * @param sem_id The index of the semaphore within the group to decrement.
- * @return 0 on success, -1 on error.
- */
 int semaphoreDown(int sem_group_id, int sem_id){
     struct sembuf values;
     values.sem_num = sem_id;
@@ -45,12 +25,6 @@ int semaphoreDown(int sem_group_id, int sem_id){
     return result;
 }
 
-/**
- * Increments the value of a specific semaphore in a semaphore group.
- * @param sem_group_id The ID of the semaphore group.
- * @param sem_id The index of the semaphore within the group to increment.
- * @return 0 on success, -1 on error.
- */
 int semaphoreUp(int sem_group_id, int sem_id){
     struct sembuf values;
     values.sem_num = sem_id;
--- semaphore.h
+++ semaphore.h
@@ -8,7 +8,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Creates a semaphore group with the specified number of semaphores.
+ * @param count The number of semaphores to create in the group.
+ * @return The semaphore group ID on success, -1 on error.
+ */
 int semaphoreCreateGroup(int count);
+
+/**
+ * Initializes the values of all semaphores in a semaphore group.
+ * @param sem_group_id The ID of the semaphore group.
+ * @param sem_id Ignored (for compatibility with future extensions).
+ * @param value The initial value to set for all semaphores in the group.
+ * @return 0 on success, -1 on error.
+ */
 int semaphoreSetValue(int sem_group_id, int sem_id, int value);
+
+/**
+ * Decrements the value of a specific semaphore in a semaphore group,
+ * potentially blocking the calling process until the value becomes non-negative.
+ *
+ * @param sem_group_id The ID of the semaphore group.
+ * @param sem_id The index of the semaphore within the group to decrement.
+ * @return 0 on success, -1 on error.
+ */
 int semaphoreDown(int sem_group_id, int sem_id);
+
+/**
+ * Increments the value of a specific semaphore in a semaphore group.
+ * @param sem_group_id The ID of the semaphore group.
+ * @param sem_id The index of the semaphore within the group to increment.
+ * @return 0 on success, -1 on error.
+ */
 int semaphoreUp(int sem_group_id, int sem_id);
