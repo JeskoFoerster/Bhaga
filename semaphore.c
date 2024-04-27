03748325c96695d58f35e4c367e9f4c0266b9004
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2024-04-27 17:03:09 +0200

    semaphore function comments

--- semaphore.c
+++ semaphore.c
@@ -2,15 +2,25 @@
 // Created by Nils Morczinietz on 27.04.2024.
 //
 
-//TEST
-
 #include "semaphore.h"
 
+/**
+ * Creates a semaphore group with the specified number of semaphores.
+ * @param count The number of semaphores to create in the group.
+ * @return The semaphore group ID on success, -1 on error.
+ */
 int semaphoreCreateGroup(int count){
     int sem_group_id = semget (IPC_PRIVATE, count, IPC_CREAT|0644);
     return sem_group_id;
 }
 
+/**
+ * Initializes the values of all semaphores in a semaphore group.
+ * @param sem_group_id The ID of the semaphore group.
+ * @param sem_id Ignored (for compatibility with future extensions).
+ * @param value The initial value to set for all semaphores in the group.
+ * @return 0 on success, -1 on error.
+ */
 int semaphoreSetValue(int sem_group_id, int sem_id, int value){
     unsigned short marker[1];
     marker[sem_id] = value;
@@ -18,6 +28,14 @@ int semaphoreSetValue(int sem_group_id, int sem_id, int value){
     return result;
 }
 
+/**
+ * Decrements the value of a specific semaphore in a semaphore group,
+ * potentially blocking the calling process until the value becomes non-negative.
+ *
+ * @param sem_group_id The ID of the semaphore group.
+ * @param sem_id The index of the semaphore within the group to decrement.
+ * @return 0 on success, -1 on error.
+ */
 int semaphoreDown(int sem_group_id, int sem_id){
     struct sembuf values;
     values.sem_num = sem_id;
@@ -27,6 +45,12 @@ int semaphoreDown(int sem_group_id, int sem_id){
     return result;
 }
 
+/**
+ * Increments the value of a specific semaphore in a semaphore group.
+ * @param sem_group_id The ID of the semaphore group.
+ * @param sem_id The index of the semaphore within the group to increment.
+ * @return 0 on success, -1 on error.
+ */
 int semaphoreUp(int sem_group_id, int sem_id){
     struct sembuf values;
     values.sem_num = sem_id;
